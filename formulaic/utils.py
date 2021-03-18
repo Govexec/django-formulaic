65d23872a564959cc737d7af15c6e7ec322275e7
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-03-18 14:17:34 -0500

    Revert "Update utils.py"
    This reverts commit 19a6ffe4da03d7bdda71cdd6443e0d6c0e471c49.

--- formulaic/utils.py
+++ formulaic/utils.py
@@ -45,7 +45,7 @@ def send_file(request, filename, full_path):
         response['Content-Length'] = os.path.getsize(full_path)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
         response.set_cookie("fileDownload", value="true", max_age=60*60, path="/")
-    return response
+        return response
 
 
 def state_from_zip(zipcode):
