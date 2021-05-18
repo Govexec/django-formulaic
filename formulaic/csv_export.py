73795f8df9aecbc0410e587375a986c1766dfc90
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-05-18 14:04:12 -0500

    Update csv_export.py

--- formulaic/csv_export.py
+++ formulaic/csv_export.py
@@ -32,9 +32,6 @@ def export_submissions_to_file(form, output_file):
                 date_created_aware = submission.date_created
             row["date"] = date_created_aware.strftime('%m/%d/%Y %H:%M')
             row["source"] = submission.source
-            # row_batch.append(
-            #     {k: u(str(v)) if isinstance(v, bool) else v
-            #      for (k, v) in row.items()}
-            # )
+            row['promo_source'] = submission.promo_source
             row_batch.append({k: u(v) for (k, v) in row.items()})
         writer.writerows(row_batch)
