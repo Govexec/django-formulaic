6f78575ff4ed3bbd4c72489e968e032e51cfcba4
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-05-24 15:54:24 -0500

    Update views.py

--- formulaic/views.py
+++ formulaic/views.py
@@ -25,7 +25,7 @@ class CustomDjangoModelPermissions(permissions.DjangoModelPermissions):
     }
 
 
-@shared_task
+@shared_task(bind=True)
 def download_submission_method(request):
     form_id = request.GET.get('form', None)
 
@@ -52,24 +52,7 @@ def download_submission_method(request):
 def download_submissions(request):
     # TODO: auto-cleanup files
 
-    form_id = request.GET.get('form', None)
-
-    if not form_id:
-        raise Http404()
-
-    try:
-        form = models.Form.objects.get(pk=form_id)
-    except models.Form.DoesNotExist:
-        raise Http404()
-
-    datetime_slug = datetime.now().strftime("%Y%m%d-%H:%M:%S-%f")
-    filename = '{}-submissions-{}.csv'.format(form.slug, datetime_slug)
-    full_path = '{}/{}'.format(settings.FORMULAIC_EXPORT_STORAGE_LOCATION, filename)
-
-    with open(full_path, 'w') as csvfile:
-        export_submissions_to_file(form, csvfile)
-
-    return utils.send_file(request, filename, full_path)
+    download_submission_method.delay(request)
 
 
 class SubmissionSourceView(rf_views.APIView):
