1a38b5d5b778505bf605f28ff7fc8515d7445f8d
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-05-24 19:47:21 -0500

    Update views.py

--- formulaic/views.py
+++ formulaic/views.py
@@ -45,6 +45,7 @@ def download_submissions(request):
 
     with open(full_path, 'w') as csvfile:
         export_submissions_to_file(form, csvfile)
+      
     return utils.send_file(request, filename, full_path)
 
 
