01900480697babe25e6df5e56dc5039a1687250e
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-05-27 16:22:26 -0500

    different way to add tags

---
+++ formulaic/templatetags/setting_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag('formulaic_tinymce_key')
+def formulaic_tinymce_key():
+    return settings.FORMULAIC_TINYMCE_KEY
+
