9e92d387dba4394c48d266cbf8e88372e6472a3c
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-05-27 16:26:01 -0500

    Update setting_tags.py

--- formulaic/templatetags/setting_tags.py
+++ formulaic/templatetags/setting_tags.py
@@ -4,7 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.simple_tag('formulaic_tinymce_key')
+@register.simple_tag(name='formulaic_tinymce_key')
 def formulaic_tinymce_key():
     return settings.FORMULAIC_TINYMCE_KEY
 
