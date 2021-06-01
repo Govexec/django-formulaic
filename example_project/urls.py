259a71d1cebd3a430a443672139d7e21cdf2c64c
Connor Ford <cpf006@gmail.com>
2021-05-31 23:52:43 -0400

    Move example project up one level

---
+++ example_project/urls.py
@@ -0,0 +1,30 @@
+"""example_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('form-test/', views.test_formulaic_form),
+    path(
+        'form-test/completed/',
+        views.test_formulaic_form_complete,
+        name='form-complete'
+    ),
+
+    path('formulaic/', include('formulaic.urls')),
+    path('admin/', admin.site.urls),
+]
