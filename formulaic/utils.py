ba7be11aaef816525254dcf7fa179feed27697c8
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-08-06 15:57:03 +0200

    Update utils.py

--- formulaic/utils.py
+++ formulaic/utils.py
@@ -46,7 +46,7 @@ def send_file(request, filename, full_path):
     # response['Content-Length'] = os.path.getsize(full_path)
     response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
     response.set_cookie("fileDownload", value="true", max_age=60*60, path="/")
-    yield response
+    return response
 
 
 def state_from_zip(zipcode):
