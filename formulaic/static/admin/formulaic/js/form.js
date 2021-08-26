8181f35263088f29238da426e62b8e930bdf9f74
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-08-26 19:43:30 +0200

    moved javascript

--- formulaic/static/admin/formulaic/js/form.js
+++ formulaic/static/admin/formulaic/js/form.js
@@ -26,3 +26,48 @@ var content_changed = false;
 
 	});
  }(django.jQuery));
+
+
+  const allTags = document.getElementsByClassName('form-download-tag')
+  allTags.map(item => {
+    (item).on('click', function(e) {
+      const attribute = item.getAttribute('data-target-id')
+      e.preventDefault();
+      const url = `/download/submissions/?form=${attribute}`;
+      $.get(url)
+        .done(function pollAsyncResults(data) {
+          context: this
+          const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
+          $.get(pollAsyncUrl)
+            .done(function(asyncData, status, xhr) {
+              context: this
+              if (xhr.status !== 202) {
+                clearTimeout(pollAsyncResults);
+                const a = document.createElement('a');
+                document.body.appendChild(a);
+                a.style='display: none';
+                a.href=asyncData.location;
+                a.download=asyncData.filename;
+                a.click();
+
+                $('#download-reports-button').text('Download Report(CSV)')
+              }
+              else {
+                $('#download-reports-button').text('Loading...')
+
+                setTimeout(function() { pollAsyncResults(data) }, 500);
+              }
+            })
+
+            .fail(function(xhr, status, error) {
+              clearTimeout(pollAsyncResults);
+
+            })
+        })
+        .fail(function(xhr, status, error) {
+
+        })
+    })
+  })
+
+
