9c8760afae5e5cf31a201e9506c1363060d95d33
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-08-27 15:43:38 +0200

    Update form.js

--- formulaic/static/admin/formulaic/js/form.js
+++ formulaic/static/admin/formulaic/js/form.js
@@ -23,53 +23,49 @@ var content_changed = false;
                 });
             });
         }
+    setTimeout(handleDownloadTags, 100)
+        
+    function handleDownloadTags(){
+        let allTags = document.getElementsByClassName('form-download-tag')
+            for( let item of allTags){
+              item.addEventListener('click', function(e) {
+              const attribute = item.getAttribute('data-target-id')
+              e.preventDefault();
+              const url = `/download/submissions/?form=${attribute}`;
+              $.get(url)
+                .done(function pollAsyncResults(data) {
+                  context: this
+                  const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
+                  $.get(pollAsyncUrl)
+                    .done(function(asyncData, status, xhr) {
+                      context: this
+                      if (xhr.status !== 202) {
+                        clearTimeout(pollAsyncResults);
+                        const a = document.createElement('a');
+                        document.body.appendChild(a);
+                        a.style='display: none';
+                        a.href=asyncData.location;
+                        a.download=asyncData.filename;
+                        a.click();
 
+                      }
+                      else {
+
+                        setTimeout(function() { pollAsyncResults(data) }, 500);
+                      }
+                    })
+
+                    .fail(function(xhr, status, error) {
+                      clearTimeout(pollAsyncResults);
+
+                    })
+                })
+                .fail(function(xhr, status, error) {
+
+                })
+            })
+            }
+    }
 	});
  }(django.jQuery));
 
-setTimeout(handleDownloadTags, 100)
-
-
-
-
-function handleDownloadTags(){
-    let allTags = document.getElementsByClassName('form-download-tag')
-        for( let item of allTags){
-          item.addEventListener('click', function(e) {
-          const attribute = item.getAttribute('data-target-id')
-          e.preventDefault();
-          const url = `/download/submissions/?form=${attribute}`;
-          $.get(url)
-            .done(function pollAsyncResults(data) {
-              context: this
-              const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
-              $.get(pollAsyncUrl)
-                .done(function(asyncData, status, xhr) {
-                  context: this
-                  if (xhr.status !== 202) {
-                    clearTimeout(pollAsyncResults);
-                    const a = document.createElement('a');
-                    document.body.appendChild(a);
-                    a.style='display: none';
-                    a.href=asyncData.location;
-                    a.download=asyncData.filename;
-                    a.click();
-
-                  }
-                  else {
-
-                    setTimeout(function() { pollAsyncResults(data) }, 500);
-                  }
-                })
-
-                .fail(function(xhr, status, error) {
-                  clearTimeout(pollAsyncResults);
-
-                })
-            })
-            .fail(function(xhr, status, error) {
-
-            })
-        })
-        }
-}
