86151f7842a5bb2ec3383af9662236ac70a359c7
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-08-27 16:00:17 +0200

    Update form.js

--- formulaic/static/admin/formulaic/js/form.js
+++ formulaic/static/admin/formulaic/js/form.js
@@ -35,7 +35,7 @@ var content_changed = false;
               $.get(url)
                 .done(function pollAsyncResults(data) {
                   context: this
-                  const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
+                  const pollAsyncUrl = `formulaic/api/poll_async_results/${data.task_id}`
                   $.get(pollAsyncUrl)
                     .done(function(asyncData, status, xhr) {
                       context: this
