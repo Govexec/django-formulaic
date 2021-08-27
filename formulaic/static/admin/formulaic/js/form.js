27d05a7a48bb0e169259f1c284ed4f1c50dd1546
levimoore1992 <30597682+levimoore1992@users.noreply.github.com>
2021-08-27 14:18:12 +0200

    Update form.js

--- formulaic/static/admin/formulaic/js/form.js
+++ formulaic/static/admin/formulaic/js/form.js
@@ -26,50 +26,47 @@ var content_changed = false;
 
 	});
  }(django.jQuery));
-
-    let allTags = document.getElementsByClassName('form-download-tag')
-    let count = 0
-    while(!allTags.length || count < 20 ){
-          let allTags = document.getElementsByClassName('form-download-tag')
-            count++
-    }
-  allTags.map(item => {
-    (item).on('click', function(e) {
-      const attribute = item.getAttribute('data-target-id')
-      e.preventDefault();
-      const url = `/download/submissions/?form=${attribute}`;
-      $.get(url)
-        .done(function pollAsyncResults(data) {
-          context: this
-          const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
-          $.get(pollAsyncUrl)
-            .done(function(asyncData, status, xhr) {
+    setTimeout(() => {
+        let allTags = document.getElementsByClassName('form-download-tag')
+        allTags.map(item => {
+        (item).on('click', function(e) {
+          const attribute = item.getAttribute('data-target-id')
+          e.preventDefault();
+          const url = `/download/submissions/?form=${attribute}`;
+          $.get(url)
+            .done(function pollAsyncResults(data) {
               context: this
-              if (xhr.status !== 202) {
-                clearTimeout(pollAsyncResults);
-                const a = document.createElement('a');
-                document.body.appendChild(a);
-                a.style='display: none';
-                a.href=asyncData.location;
-                a.download=asyncData.filename;
-                a.click();
+              const pollAsyncUrl = `/api/poll_async_results/${data.task_id}`
+              $.get(pollAsyncUrl)
+                .done(function(asyncData, status, xhr) {
+                  context: this
+                  if (xhr.status !== 202) {
+                    clearTimeout(pollAsyncResults);
+                    const a = document.createElement('a');
+                    document.body.appendChild(a);
+                    a.style='display: none';
+                    a.href=asyncData.location;
+                    a.download=asyncData.filename;
+                    a.click();
 
-              }
-              else {
+                  }
+                  else {
 
-                setTimeout(function() { pollAsyncResults(data) }, 500);
-              }
+                    setTimeout(function() { pollAsyncResults(data) }, 500);
+                  }
+                })
+
+                .fail(function(xhr, status, error) {
+                  clearTimeout(pollAsyncResults);
+
+                })
             })
-
             .fail(function(xhr, status, error) {
-              clearTimeout(pollAsyncResults);
 
             })
         })
-        .fail(function(xhr, status, error) {
-
-        })
+    }, 100)
     })
-  })
+
 
 
