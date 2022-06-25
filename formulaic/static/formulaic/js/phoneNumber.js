fe8f362077ae9ffb9e763abfbbfa88c0bdd4339c
Lachlan Bell <lbell@govexec.com>
2022-06-25 12:14:44 -0400

    VIR-45 Cleaning up JS

--- formulaic/static/formulaic/js/phoneNumber.js
+++ formulaic/static/formulaic/js/phoneNumber.js
@@ -1,40 +1,4 @@
 /*********** Phone Number Widget ***********/
-// let resetErrors = function(phoneElement){
-//     // Remove all errors
-//     let wrapperDiv = phoneElement.closest(".field-wrapper");
-//     wrapperDiv.classList.remove("field-error");
-//     wrapperDiv.classList.remove("formulaic-error");
-//     wrapperDiv.classList.remove("has-error");
-//
-//     let span = wrapperDiv.querySelector(".error-message");
-//     if(span) span.remove();
-//
-// }
-//
-// let addRemoveErrors = function(iti, phoneElement){
-//     // Add or remove errors on the form.
-//
-//     let wrapperDiv = phoneElement.closest(".field-wrapper");
-//     if (iti.isValidNumber()) {
-//         // A Valid number gets reformatted.
-//         resetErrors(phoneElement);
-//         iti.setNumber(iti.getNumber());
-//
-//     } else {
-//         // Add the error classes to the field.
-//         wrapperDiv.classList.add("field-error");
-//         wrapperDiv.classList.add("formulaic-error");
-//         wrapperDiv.classList.add("has-error");
-//
-//         // Add an error span with message.
-//         let span = document.createElement("span")
-//         span.append("Enter a valid phone number.")
-//         span.classList.add("error-message");
-//         wrapperDiv.appendChild(span);
-//     }
-// }
-
-
 $(document).ready(function () {
     // https://github.com/jackocnr/intl-tel-input
     // Assign the utilsScript to all phone inputs.
@@ -64,22 +28,5 @@ $(document).ready(function () {
             // Nicely format the number if it is valid.
             if (iti.isValidNumber()) iti.setNumber(iti.getNumber());
         });
-
-
-        // Not sure we want to do this, as it does not generalize too well...
-
-        // // On blur, check if it seems like it is a valid phone number.
-        // phoneElement.addEventListener('blur', function() {
-        //     // If blank, return early.
-        //     if (phoneElement.value.trim().length === 0) return;
-        //     addRemoveErrors(iti, phoneElement);
-        // });
-        //
-        // // Clear errors on focus.
-        // phoneElement.addEventListener('focus', function() {
-        //     resetErrors(phoneElement);
-        // });
-        //
-        // // if (phoneElement.value.trim()) addRemoveErrors(iti, phoneElement);
     }
 });
